e48d6e04dd3ea67aa17f75ebfaac99fd09e8ff7e
nodeJun <ksh03196@gmail.com>
2025-09-04 17:19:49 +0900

    fix(front): 응답값 수정

--- src/ui/shared/form-field/ContractDocument/uploadFile.ts
+++ src/ui/shared/form-field/ContractDocument/uploadFile.ts
@@ -2,7 +2,7 @@ import { uploadFile as uploadFileAPI } from '@shared/api'
 
 const uploadFile = async (file: File) => {
   const response = await uploadFileAPI(file)
-  return response.id
+  return response.contractDocumentId
 }
 
 export default uploadFile
